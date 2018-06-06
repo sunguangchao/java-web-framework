c14a8d063da9217ba20bf603b31ace4542eb57a6
sunguangchao <1198125767@qq.com>
2018-06-06 21:28:20 +0800

    add
    Signed-off-by: sunguangchao <1198125767@qq.com>

--- ThreadLocal/DBUtil.java
+++ ThreadLocal/DBUtil.java
@@ -37,3 +37,59 @@ public class DBUtil{
 		}
 	}
 }
+
+
+//使用：作为一个布尔状态标志，用于指示发生了一个重要的一次性事件，例如完成初始化或任务结束
+//使用理由：状态标志并不依赖于程序内任何其他状态，且通常只有一种状态转换
+//例子：判断业务是否结束
+volatile  boolean isOK = false;
+public void isOK(){
+	isOK = true;
+}
+
+public void doWork(){
+	//循环监听状态位变化
+	while (isOK) {
+		//do work
+	}
+}
+
+//使用：当读远多于写，结合使用内部锁和 volatile 变量来减少同步的开销
+//使用理由：利用volatile保证读取操作的可见性；利用synchronized保证复合操作的原子性
+@ThreadSafe
+public class Counter{
+	private volatile int value;
+	//利用volatile保证读取操作的可见性
+	public int getValue(){
+		return vlaue;
+	}
+	//利用synchronized保证复合操作的原子性
+	public synchronized int increment(){
+		return value++;
+	}
+}
+
+//双重检查锁定：实现线程安全的延迟初始化，同时降低同步开销
+//    1.多线程并发创建对象时，会通过加锁保证只有一个线程能创建对象
+//    2.对象创建完毕，执行get方法将不需要获取锁，直接返回创建对象
+//
+public class Singleton {
+	//通过volatile声明，实现线程安全的延迟初始化
+    private volatile static Singleton singleton;
+    
+    private Singleton(){}
+    //双重锁设计
+    public static Singleton getInstance(){
+    	//1.多线程并发创建对象时，会通过加锁保证只有一个线程能创建对象
+        if (singleton == null){
+            synchronized (Singleton.class){
+                if (singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        //2.对象创建完毕，执行get方法将不需要获取锁，直接返回创建对象
+        return singleton;
+    }
+}
+
